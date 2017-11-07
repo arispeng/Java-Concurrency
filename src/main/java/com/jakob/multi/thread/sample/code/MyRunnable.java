ed82d5d37caff0c1b2dbca9a13f385fea4f82fb1
Peng <cpeng200@cable.comcast.com>
2017-11-07 13:56:01 -0500

    add new files

---
+++ src/main/java/com/jakob/multi/thread/sample/code/MyRunnable.java
@@ -0,0 +1,7 @@
+package com.jakob.multi.thread.sample.code;
+
+public class MyRunnable implements Runnable {
+	public void run() {
+		System.out.println("MyRunnable is running");
+	}
+}
---
+++ src/main/java/com/jakob/multi/thread/sample/code/MyThread.java
@@ -0,0 +1,9 @@
+package com.jakob.multi.thread.sample.code;
+
+public class MyThread extends Thread {
+	public void run() {
+		System.out.println("MyThread running");
+	}
+
+
+}
